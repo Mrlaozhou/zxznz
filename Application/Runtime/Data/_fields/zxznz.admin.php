19fb0f90ebc9d0e484c721ee13ad98886e20da61
Mrlaozhou <zgs5999@163.com>
2017-03-10 11:52:56 +0800

    to safe

--- Application/Runtime/Data/_fields/zxznz.admin.php
+++
@@ -1,1 +0,0 @@
-a:5:{i:0;s:2:"id";i:1;s:8:"username";i:2;s:8:"password";s:3:"_pk";s:2:"id";s:5:"_type";a:3:{s:2:"id";s:16:"int(10) unsigned";s:8:"username";s:11:"varchar(30)";s:8:"password";s:11:"varchar(32)";}}
