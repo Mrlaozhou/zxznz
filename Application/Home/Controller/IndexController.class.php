2567b8028a3beae719c81f88e37815ad41a3f55f
Mrlaozhou <zgs5999@163.com>
2017-03-10 11:49:51 +0800

    null

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,7 +1,8 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-class IndexController extends Controller {
+class IndexController extends Controller 
+{
 
     public function index()
     {
